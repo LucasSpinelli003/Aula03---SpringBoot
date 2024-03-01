1bb04860336a42add9186eadd6e4e652a731730a
LucasSpinelli003 <rm97757@fiap.com.br>
2024-02-29 22:54:25 -0300

    feat - post and dto(like typescript interface) created

--- src/main/java/br/com/fiap/Aula03/controller/MercadoController.java
+++ src/main/java/br/com/fiap/Aula03/controller/MercadoController.java
@@ -1,15 +1,23 @@
 package br.com.fiap.Aula03.controller;
 
+import br.com.fiap.Aula03.dto.CadastroMercadoDto;
 import br.com.fiap.Aula03.model.CategoriaMercado;
 import br.com.fiap.Aula03.model.Mercado;
 import jakarta.websocket.server.PathParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@RestController("/mercado")
+@RestController
+@RequestMapping("/mercados")
 public class MercadoController {
 
+    @PostMapping
+    public ResponseEntity<Mercado> cadastrar(@RequestBody CadastroMercadoDto mercadoDto, UriComponentsBuilder   uri){
+    var url = uri.path("/mercados/{id}").buildAndExpand(1).toUri();
+    return ResponseEntity.created(url).body(new Mercado(1,"Sonda",CategoriaMercado.SUPER));
+    }
     @GetMapping
     public Mercado get(){
         return new Mercado(1, "Assai", CategoriaMercado.HIPER);
---
+++ src/main/java/br/com/fiap/Aula03/dto/CadastroMercadoDto.java
@@ -0,0 +1,8 @@
+package br.com.fiap.Aula03.dto;
+
+import br.com.fiap.Aula03.model.CategoriaMercado;
+
+
+
+public record CadastroMercadoDto(String nome, CategoriaMercado categoria) {
+}
